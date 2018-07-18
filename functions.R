3d85c36f246c6059606b74e06a66b2439dbd70cd
hkaspersen <hakon.kaspersen@protonmail.com>
2018-07-18 11:22:31 +0200

    added functions for plot and folder check

--- functions.R
+++ functions.R
@@ -1,4 +1,9 @@
-data_wrangle <- function(df) {
+check_dir <- function(output_dir) {
+  folder <- paste0("results_", Sys.Date())
+  dir.create(file.path(output_dir, folder), showWarnings = FALSE)
+}
+
+create_report <- function(df) {
   df <- df %>%
     rename(ctg_0_bp = X..contigs.....0.bp.,
            ctg_1000_bp = X..contigs.....1000.bp.,
@@ -25,7 +30,7 @@ data_wrangle <- function(df) {
   return(df)
 }
 
-create_boxplot_reports <- function(df) {
+create_boxplot_report <- function(df) {
   df <- df %>%
     gather(key_contig,
            value_contig,
@@ -50,19 +55,152 @@ create_boxplot_reports <- function(df) {
   return(df)
 }
 
-filter_cutoffs <- function(x) {
-  max <- max(x)
-  third <- quantile(x, 0.75)[[1]]
-  min <- min(x)
+## Plots
+
+create_plots <- function(df) {
+  cols <- c("#e7f0fa","#c9e2f6",
+            "#95cbee","#0099dc",
+            "#4ab04a", "#ffd73e",
+            "#eec73a","#e29421",
+            "#f05336","#ce472e")
   
-  dist_max <- abs(third - max)
-  dist_min <- abs(third - min)
+  p1 <- ggplot(df, aes(Assembly, no_of_contigs))+
+    geom_col(color = "black")+
+    theme_classic()+
+    labs(y = "# of contigs")+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3),
+          axis.title.x = element_blank())
   
-  df <- c()
+  p2 <- ggplot(df, aes(Assembly, no_of_misassemblies))+
+    geom_col(color = "black")+
+    theme_classic()+
+    labs(title = "Misassembled contigs",
+         y = "# of contigs")+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3),
+          axis.title.x = element_blank())
   
-  if (dist_max > dist_min) {
-    df <- append(df, third)
-  }
+  p3 <- ggplot(df, aes(Assembly, N_per_100kbp))+
+    geom_col(color = "black")+
+    theme_classic()+
+    labs(title = "Ns per 100 kbp",
+         y = "# of Ns")+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3),
+          axis.title.x = element_blank())
   
-  return(df)
+  p4 <- ggplot(df, aes(N50, Largest.contig, fill = no_of_contigs))+
+    geom_point(pch = 21,
+               size = 3)+
+    scale_fill_gradientn(colors=cols,na.value="black",
+                         limits=c(0,500),
+                         guide=guide_colourbar(ticks=T,nbin=50,
+                                               barheight=.5,label=T, 
+                                               barwidth=10))+
+    theme_classic()+
+    labs(y = "Largest Contig",
+         fill = "Number of Contigs")+
+    theme(legend.position = "bottom")
+  
+  ggsave(paste0(output_dir,"/assembly_contigs.tiff"),
+         p1,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
+  
+  ggsave(paste0(output_dir,"/assembly_misassemblies.tiff"),
+         p2,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
+  
+  ggsave(paste0(output_dir,"/assembly_Ns.tiff"),
+         p3,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
+  
+  ggsave(paste0(output_dir,"/N50_largest_contigs.tiff"),
+         p4,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
 }
+
+create_boxplots <- function(df) {
+  p1 <- ggplot(df, aes(key_contig, value_contig))+
+    geom_boxplot()+
+    theme_classic()+
+    labs(y = "# of contigs")+
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  p2 <- ggplot(df, aes(key_total, value_total))+
+    geom_boxplot()+
+    theme_classic()+
+    labs(y = "Total size of contigs")+
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  p3 <- ggplot(df, aes(key_ng, value_ng))+
+    geom_boxplot()+
+    theme_classic()+
+    labs(y = "# of contigs")+
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  ggsave(paste0(output_dir,"/no_of_contigs_per_size.tiff"),
+         p1,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
+  
+  ggsave(paste0(output_dir,"/assembly_contig_sizes.tiff"),
+         p2,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
+  
+  ggsave(paste0(output_dir,"/N50_NG50.tiff"),
+         p3,
+         dpi = 100,
+         units = "cm",
+         device = "tiff",
+         height = 20,
+         width = 25)
+}
+
+
+# Data
+save_data <- function(df) {
+  df <- df %>%
+    select(Assembly,
+           no_of_contigs,
+           Largest.contig,
+           Total.length,
+           N50,
+           GC....,
+           no_of_misassemblies,
+           N_per_100kbp,
+           Largest.alignment) %>%
+    rename(`GC%` = GC....,
+           "Number of contigs" = no_of_contigs,
+           "Largest contig" = Largest.contig,
+           "Total length" = Total.length,
+           "Number of misassemblies" = no_of_misassemblies,
+           "N per 100 kbp" = N_per_100kbp,
+           "Largest alignment" = Largest.alignment)
+  
+  write.xlsx(df, paste0(output_dir, "/selected_results.xlsx"))
+}
+
