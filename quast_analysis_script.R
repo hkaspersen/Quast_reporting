7e217ac5f3c4025d772458593a0ad3e2a3c87158
hkaspersen <hakon.kaspersen@protonmail.com>
2018-07-18 11:23:13 +0200

    added use of the functions and output prompt

--- quast_analysis_script.R
+++ quast_analysis_script.R
@@ -1,8 +1,13 @@
 library(tidyverse)
+library(openxlsx)
 
 source("functions.R")
 
 report_loc <- readline(prompt = "Please supply location of transposed_report.tsv (use '/' instead of '\'): ")
+output_dir <- readline(prompt = "Please supply output location: ")
+
+check_dir(output_dir)
+output_dir <- paste0(output_dir, "/results_", Sys.Date())
 
 
 raw_report <- read.delim(paste0(report_loc,
@@ -11,56 +16,13 @@ raw_report <- read.delim(paste0(report_loc,
                          header = TRUE,
                          stringsAsFactors = F)
 
+# Data wrangling
+clean_report <- create_report(raw_report)
+boxplot_report <- create_boxplot_report(clean_report)
 
-clean_report <- data_wrangle(raw_report)
-boxplot_report <- create_boxplot_reports(clean_report)
+# Plotting
+create_plots(clean_report)
+create_boxplots(boxplot_report)
 
-# Contig sizes
-ggplot(boxplot_report, aes(key_contig, value_contig))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(y = "# of contigs")+
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1))
-
-# Total sizes
-ggplot(boxplot_report, aes(key_total, value_total))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(y = "# of contigs")+
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1))
-
-# N50
-ggplot(boxplot_report, aes(key_ng, value_ng))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(y = "# of contigs")+
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1))
-
-# Number of contigs per assembly
-ggplot(clean_report, aes(Assembly, no_of_contigs))+
-  geom_col(color = "black")+
-  theme_classic()+
-  labs(y = "# of contigs")+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3),
-        axis.title.x = element_blank())
-
-# Misassemlies
-ggplot(clean_report, aes(Assembly, no_of_misassemblies))+
-  geom_col(color = "black")+
-  theme_classic()+
-  labs(title = "Misassembled contigs",
-       y = "# of contigs")+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3),
-        axis.title.x = element_blank())
-
-# N per 100 kbps
-ggplot(clean_report, aes(Assembly, N_per_100kbp))+
-  geom_col(color = "black")+
-  theme_classic()+
-  labs(title = "Ns per 100 kbp",
-       y = "# of Ns")+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3),
-        axis.title.x = element_blank())
+# Data
+save_data(clean_report)
