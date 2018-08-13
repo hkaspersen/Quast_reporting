64cdec98451aa7f3550ed5dc2f9085174e582ffe
hkaspersen <hakon.kaspersen@protonmail.com>
2018-08-13 13:46:00 +0200

    updated boxplots with whiskers

--- quast_analysis_script.R
+++ quast_analysis_script.R
@@ -222,7 +222,7 @@ save_data <- function(df) {
 # Run functions
 check_dir(output_dir)
 
-output_dir <- paste0(output_dir, "/multiQuast_results_", Sys.Date())
+output_dir <- paste0(output_dir, "/results_", Sys.Date())
 
 raw_report <- read.delim(report_loc,
                          sep = "\t",
